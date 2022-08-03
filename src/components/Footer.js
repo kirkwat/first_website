2bcb16e7aa8b3c74c7aed0007796cb9d17fd10c2
kirkwat <watson.kirk00@gmail.com>
2022-08-02 23:43:08 -0500

    footer improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import styles from "../styles/Footer.module.css";
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.container}>
-        Designed, Developed, and Maintained by Kirk Watson
+      <div className={styles.socialMedia}>
+        <GitHubIcon onClick={event =>  window.location.href='https://github.com/kirkwat'}/>
+        <LinkedInIcon onClick={event =>  window.location.href='https://www.linkedin.com/in/kirk-watson/'}/>
       </div>
+      <p >
+        Developed and Maintained by Kirk Watson
+      </p>
     </div>
   );
 }
