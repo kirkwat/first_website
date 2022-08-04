ac79d2043ae872372949b4a71b7d3e6018123f60
kirkwat <watson.kirk00@gmail.com>
2022-08-03 22:46:13 -0500

    clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 import React from "react";
+import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import styles from "../styles/Footer.module.css";
+
+//TO ADD
+  //enlarge buttons when hovering
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.socialMedia}>
-        <GitHubIcon onClick={event =>  window.location.href='https://github.com/kirkwat'}/>
-        <LinkedInIcon onClick={event =>  window.location.href='https://www.linkedin.com/in/kirk-watson/'}/>
+        <IconButton
+          onClick={event =>  window.location.href='https://github.com/kirkwat'}
+        >
+          <GitHubIcon/>
+        </IconButton>
+        <IconButton
+          onClick={event =>  window.location.href='https://www.linkedin.com/in/kirk-watson/'}
+        >
+          <LinkedInIcon/>
+        </IconButton>
       </div>
       <p >
         Developed and Maintained by Kirk Watson
